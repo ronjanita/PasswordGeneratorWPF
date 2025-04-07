91513a018927e0911423a76e609333ae6df901aa
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-04-07 10:44:10 +0200

    created first textblock for welcome message

--- PasswordGeneratorWPF/MainWindow.xaml.cs
+++ PasswordGeneratorWPF/MainWindow.xaml.cs
@@ -24,5 +24,15 @@ namespace PasswordGeneratorWPF
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void CheckBox_Checked(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
