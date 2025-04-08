29d39a3cf617050ffcf67742c0dcbad3707c2e92
Ronja Nilsson <ronja.nilsson@autismuslink.ch>
2025-04-08 16:14:54 +0200

    discused with daniel and finished everything

--- PasswordGeneratorWPF/MainWindow.xaml.cs
+++ PasswordGeneratorWPF/MainWindow.xaml.cs
@@ -43,32 +43,80 @@ namespace PasswordGeneratorWPF
         {
             Finalpassword.Text = password;
         }
+        //private string GeneratePassword(int length, bool Uppercase, bool Digits, bool Symbols, string required)
+        //{
+        //    string lowercase = "abcdefghijklmnopqrstuvwxyz";
+        //    string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        //    string numbers = "0123456789";
+        //    string symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+
+        //    string charPool = lowercase;
+        //    if (Uppercase) charPool += uppercase;
+        //    if (Digits) charPool += numbers;
+        //    if (Symbols) charPool += symbols;
+
+        //    List<char> passwordChars = new List<char>();
+
+        //    if (!string.IsNullOrEmpty(required))
+        //    {
+        //            passwordChars.AddRange(required);
+        //            length -= required.Length;
+        //    }
+
+        //    Random rand = new Random();
+
+        //    for (int i = 0; i < length; i++)
+        //    {
+        //        passwordChars.Add(charPool[rand.Next(charPool.Length)]);
+        //    }
+        //    return new string(passwordChars.ToArray());
+        //}
         private string GeneratePassword(int length, bool Uppercase, bool Digits, bool Symbols, string required)
         {
-            string lowercase = "abcdefghijklmnopqrstuvwxyz";
-            string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            string numbers = "0123456789";
-            string symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+            const string lowercase = "abcdefghijklmnopqrstuvwxyz";
+            const string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            const string digits = "0123456789";
+            const string symbols = "!@#$%^&*()_+-=[]{}|;:,.<>?";
+            string availableCharacters = lowercase;
 
-            string charPool = lowercase;
-            if (Uppercase) charPool += uppercase;
-            if (Digits) charPool += numbers;
-            if (Symbols) charPool += symbols;
+            string passwordToReturn = "";
 
-            Random rand = new Random();
-            List<char> passwordChars = new List<char>();
-
-            if (!string.IsNullOrEmpty(required))
+            if (Uppercase)
             {
-                passwordChars.Add(required[0]);
-                length--;
+                availableCharacters = availableCharacters + uppercase;
+            }
+            if (Digits)
+            {
+                availableCharacters = availableCharacters + digits;
+            }
+            if (Symbols)
+            {
+                availableCharacters = availableCharacters + symbols;
             }
 
-            for (int i = 0; i < length; i++)
+            if (required != "")   //überprüfung ob useringabe NICHt leer ist, falls ja, gehts weiter, falls nein wird dieser teil 
             {
-                passwordChars.Add(charPool[rand.Next(charPool.Length)]);
+                passwordToReturn = passwordToReturn + required;
+                //for (int characterPosition = 0; characterPosition < required.Length; characterPosition++)  //jedes zeichen von userinput wird durch gegangen
+                //{
+                //    passwordChars.Add(required[characterPosition]);  //jedes zeichen wird einzel in liste hinzugefügt aber bleiben in der reihenfolge wie sie in required stehen
+                //}
+                length = length - required.Length;  //wenn user z.b 5 buchstaben eingbit und am anfang ein 12 charater langes wort will, müssen nurnoch 7 random erstellt werden
             }
-            return new string(passwordChars.OrderBy(_ => rand.Next()).ToArray());
+
+            Random random = new Random();
+
+            while (length > 0)
+            {
+                int randomIndex = random.Next(0, availableCharacters.Length);
+
+
+                passwordToReturn = passwordToReturn + availableCharacters[randomIndex];
+                length = length - 1;
+            }
+
+            
+            return passwordToReturn;
         }
     }
 }
